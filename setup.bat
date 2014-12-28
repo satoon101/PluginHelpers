e69e4319cb7faa854476565eda21e4b9e817dc91
Stephen Toon <satoon101@gmail.com>
2014-12-28 12:14:11 -0500

    Moved all batch file, except setup.bat, to plugin_helpers/windows/ along with the default.ini file.
    setup.bat now copies all batch files over.

--- setup.bat
+++ setup.bat
@@ -3,16 +3,26 @@
 :: Set the start directory for later reference
 set STARTDIR="%CD%"
 
+:: Create the batch array
+set BATCH_ARRAY[0]="plugin_checker"
+set BATCH_ARRAY[1]="plugin_creater"
+set BATCH_ARRAY[2]="plugin_linker"
+set BATCH_ARRAY[3]="plugin_releaser"
+set BATCH_ARRAY[4]="prerequisites"
+set BATCH_ARRAY[5]="sp_linker"
+
+:: Loop through all batch files
+for /F "tokens=2 delims==" %%s in ('set BATCH_ARRAY[') do (
+
+    :: Copy the file
+    copy plugin_helpers\windows\%%s.bat %%s.bat
+)
+
 :: Does the config file already exist?
 if not exist %STARTDIR%\config.ini (
 
     echo Creating config.ini file.  Set values to your specifications.
 
     :: Copy the default config
-    copy packages\windows-defaults.ini config.ini
-    pause
-
-) else (
-    echo config.ini file already exists.  Please edit it to your liking.
-    pause
+    copy plugin_helpers\windows\defaults.ini config.ini
 )
