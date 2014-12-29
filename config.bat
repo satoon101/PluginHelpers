e3856b3901dea1d7d351100ef719c8499dd0b025
Stephen Toon <satoon101@gmail.com>
2014-12-28 21:24:47 -0500

    Added git hooks for testing purposes.

--- config.bat
+++ config.bat
@@ -4,10 +4,32 @@
 set STARTDIR="%CD%"
 
 :: Does the config file already exist?
-if not exist %STARTDIR%\config.ini (
+if exist %STARTDIR%\config.ini (
+
+    echo config.ini file already exists.  Please edit it to your liking.
+
+) else (
 
     echo Creating config.ini file.  Set values to your specifications.
 
     :: Copy the default config
-    copy plugin_helpers\windows\defaults.ini config.ini
+    copy plugin_helpers\windows-defaults.ini config.ini
 )
+
+echo.
+echo.
+
+:: Loop through all hooks
+for %%i in (%STARTDIR%\plugin_helpers\hooks\*.*) do (
+
+    :: Does the hook's link exist?
+    if not exist %STARTDIR%\.git\hooks\%%~ni (
+
+        :: Create the hook
+        mklink /H %STARTDIR%\.git\hooks\%%~ni %%i
+    )
+)
+
+git pull
+
+pause
