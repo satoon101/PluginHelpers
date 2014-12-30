3f115e663748c014ac837d6e83d7e5dd5fcded06
Stephen Toon <satoon101@gmail.com>
2014-12-29 21:12:16 -0500

    Added a requirements.txt for the prerequisite packages.

--- plugin_helpers/windows/prerequisites.bat
+++ plugin_helpers/windows/prerequisites.bat
@@ -1,34 +1,11 @@
 @echo off
 
-:: Create the prerequisite array
-set PREREQUISITEs[0]="configobj"
-set PREREQUISITEs[1]="path.py"
-set PREREQUISITEs[2]="pep8"
-set PREREQUISITEs[3]="pep257"
-set PREREQUISITEs[4]="pyflakes"
-set PREREQUISITEs[5]="pylint"
-
-
-:: Allow the use of delayed expansion
-setlocal EnableDelayedExpansion
-
 :: Execute the configuration
 call plugin_helpers/tools/exec_config
 
 :: Did the configuration encounter no errors?
 if %errorlevel% == 0 (
 
-    :: Loop through all prerequisites
-    for /F "tokens=2 delims==" %%s in ('set PREREQUISITES[') do (
-
-        :: Print a message about the prerequisite being installed
-        echo Attempting to install/upgrade %%s.
-        echo.
-
-        :: Install the prerequisite
-        %PYTHONEXE% -m pip install --upgrade %%s
-
-        echo.
-    )
+    %PYTHONEXE% -m pip install --upgrade -r plugin_helpers/tools/requirements.txt
 )
 pause
