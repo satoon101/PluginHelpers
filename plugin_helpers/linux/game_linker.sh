350dc5bb07c1bd8c4d5369bc536e5a4b000273ff
Stephen Toon <satoon101@gmail.com>
2014-12-31 11:12:35 -0500

    Added game_linker.sh and game_linker.bat files for #6.  Work on game_linker.py has not yet begun.

---
+++ plugin_helpers/linux/game_linker.sh
@@ -0,0 +1,2 @@
+# Execute the game_linker
+sh plugin_helpers/tools/call_python.sh game_linker
---
+++ plugin_helpers/windows/game_linker.bat
@@ -0,0 +1,5 @@
+@echo off
+
+:: Execute the game_linker
+call plugin_helpers/tools/call_python game_linker
+pause
