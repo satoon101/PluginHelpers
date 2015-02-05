2fc1470f489a065e1b313a3415cfc081b0617e4a
Stephen Toon <satoon101@gmail.com>
2015-02-04 23:46:15 -0500

    Updated plugin_helpers/tools/hooks.sh now that constants and functions are in the common package.

--- plugin_helpers/tools/hooks.sh
+++ plugin_helpers/tools/hooks.sh
@@ -1,24 +1,6 @@
 # Store the current directory for later use
 STARTDIR="$PWD"
 
-# Store the Python files to not make callers to
-PYTHON_EXCEPTIONS=("__init__" "constants" "functions")
-
-# A function to get whether a file name is in the Python Exceptions array
-function should_create_caller () {
-
-    local n=$#
-    local value=${!n}
-    for ((i=1;i < $#;i++)) {
-        if [ "${!i}" == "${value}" ]; then
-            echo "y"
-            return 0
-        fi
-    }
-    echo "n"
-    return 1
-}
-
 # Is the operating system Windows?
 if [ "$OSTYPE" == "msys" ]; then
     DIRECTORY='windows'
@@ -36,14 +18,17 @@ if [ ! -f $STARTDIR/prerequisites.$EXTENSION ]; then
 fi
 
 # Loop through all Python files
-for filename in ./plugin_helpers/packages/*; do
+for filename in ./plugin_helpers/packages/*.py; do
 
-    # Link a caller for the file if necessary
-    if [ ! $(should_create_caller "${PYTHON_EXCEPTIONS[@]}" "$(basename "${filename%.*}")") == "y" ]; then
-        if [ ! "$(basename "${filename%.**}")" == "" ]; then
-            if [ ! -f ./"$(basename "${filename%.*}")".$EXTENSION ]; then
-                ln ./plugin_helpers/$DIRECTORY/caller.$EXTENSION ./"$(basename "${filename%.*}")".$EXTENSION
-            fi
-        fi
+    # Skip the __init__ file
+    if [ "$(basename "${filename%.**}")" == "__init__" ]; then
+        continue
+    fi
+
+    # Has the link file not been created?
+    if [ ! -f ./"$(basename "${filename%.*}")".$EXTENSION ]; then
+
+        # Link a caller for the file
+        ln ./plugin_helpers/$DIRECTORY/caller.$EXTENSION ./"$(basename "${filename%.*}")".$EXTENSION
     fi
 done
