04f104ec39a1f2d1d00c40fb0809c5110d8d9c07
Stephen Toon <satoon101@gmail.com>
2015-08-10 09:28:55 -0400

    Fixed issue in prerequisites for Windows.

--- plugin_helpers/windows/prerequisites.bat
+++ plugin_helpers/windows/prerequisites.bat
@@ -6,6 +6,6 @@ call plugin_helpers/windows/exec_config
 :: Did the configuration encounter no errors?
 if %errorlevel% == 0 (
 
-    %PYTHONEXE% -m pip install --upgrade -r plugin_helpers/tools/requirements.txt
+    %PYTHON_EXECUTABLE% -m pip install --upgrade -r plugin_helpers/tools/requirements.txt
 )
 pause
