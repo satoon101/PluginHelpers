891a4613d530a1edfb708616c167bbfbf2684f11
Stephen Toon <satoon101@gmail.com>
2016-03-20 19:12:32 -0400

    Updated info.convar to reflect recent Source.Python changes.

--- plugin_helpers/files/info.py
+++ plugin_helpers/files/info.py
@@ -22,5 +22,4 @@ info.version = '1.0'
 info.basename = '$plugin_name'
 info.variable = info.basename + '_version'
 info.url = ''
-info.convar = PublicConVar(
-    info.variable, info.version, 0, info.name + ' Version')
+info.convar = PublicConVar(info.variable, info.version, info.name + ' Version')
