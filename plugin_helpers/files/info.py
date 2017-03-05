5b5e2c577a10c0b0878b120e39c564276ebef604
Stephen Toon <satoon101@gmail.com>
2017-03-04 22:16:51 -0500

    Changed the default info.py to use the new info.ini for recent Source.Python changes.

--- plugin_helpers/files/info.py
+++ plugin_helpers/files/info.py
@@ -5,21 +5,11 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
-# Source.Python Imports
-#   Cvars
-from cvars.public import PublicConVar
-#   Plugins
-from plugins.info import PluginInfo
+# Source.Python
+from plugins.manager import plugin_manager
 
 
 # =============================================================================
 # >> PLUGIN INFO
 # =============================================================================
-info = PluginInfo()
-info.name = '$plugin_title'
-info.author = '$author'
-info.version = '1.0'
-info.basename = '$plugin_name'
-info.variable = info.basename + '_version'
-info.url = ''
-info.convar = PublicConVar(info.variable, info.version, info.name + ' Version')
+info = plugin_manager.get_plugin_info(__name__)
